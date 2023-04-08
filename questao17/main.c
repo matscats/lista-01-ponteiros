cee5aaf5c61bcd2ed9174019a3d0a0904a06af64
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2023-04-08 14:57:27 -0300

    .

--- questao17/main.c
+++ questao17/main.c
@@ -11,7 +11,7 @@ int main(void) {
   int vec_1[] = {1,2,3};
   int vec_2[] = {2,3,4};
   int soma[3];
-  soma_vetor(vec_1, vec_2, *&soma, 3);
+  soma_vetor(vec_1, vec_2, &soma[0], 3);
   for (int i = 0; i < 3; i++) {
     printf("%d ", soma[i]);
   }
